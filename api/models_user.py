0f116462f661bceb87b559d39752ba6d7cb745c1
kingmo888 <17401091+kingmo888@users.noreply.github.com>
2023-12-14 09:10:05 +0800

    后台admin调整为只有管理员可以登录, fix #12

--- api/models_user.py
+++ api/models_user.py
@@ -74,7 +74,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     @property
     def is_staff(self):
         "Is the user a member of staff?"
-        # Simplest possible answer: All admins are staff
+        # Simplest possible answer: All admins are staff 
         return self.is_admin
 
     class Meta:
