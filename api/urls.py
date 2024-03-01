6c7cd025e6258d786de0721aa39f8efa40ef710c
Sunev <ccg6612@163.com>
2024-03-01 22:46:16 +0800

    兼容 x86-sciter 版客户端，此版客户端通过访问 "POST /api/ab/get" 来获取地址簿

--- api/urls.py
+++ api/urls.py
@@ -9,7 +9,8 @@ from api import views
 urlpatterns = [
     url(r'^login',views.login),
     url(r'^logout',views.logout),
-    url(r'^ab',views.ab),
+    url(r'^ab$',views.ab),
+    url(r'^ab\/get',views.ab_get), # 兼容 x86-sciter 版客户端
     url(r'^users',views.users),
     url(r'^peers',views.peers),
     url(r'^currentUser',views.currentUser),
