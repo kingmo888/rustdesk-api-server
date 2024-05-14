81341d5b6ddefd85d78447390048d20da43e9efc
kingmo888 <17401091+kingmo888@users.noreply.github.com>
2024-05-14 10:47:41 +0800

    Fix bugs and adjust internationalization of new pages

--- rustdesk_server_api/settings.py
+++ rustdesk_server_api/settings.py
@@ -44,6 +44,7 @@ MYSQL_PORT = os.environ.get("MYSQL_PORT", '3306')
 #==========数据库配置 结束=====================
 
 LANGUAGE_CODE = os.environ.get("LANGUAGE_CODE", 'zh-hans')
+##LANGUAGE_CODE = os.environ.get("LANGUAGE_CODE", 'en')
 
 # Application definition
 
